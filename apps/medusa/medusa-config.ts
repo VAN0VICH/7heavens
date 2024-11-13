dafaafbd6802834a204259aa52440995c8295c4c
LordPachimari <opachimari@gmail.com>
2024-11-13 13:55:49 +0300

    fix

--- apps/medusa/medusa-config.ts
+++ apps/medusa/medusa-config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv, Modules } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV!, process.cwd());
+loadEnv(process.env.NODE_ENV, process.cwd());
 
 export default defineConfig({
 	projectConfig: {
@@ -8,9 +8,9 @@ export default defineConfig({
 		databaseUrl: process.env.DATABASE_URL,
 		databaseLogging: true,
 		http: {
-			storeCors: process.env.STORE_CORS!,
-			adminCors: process.env.ADMIN_CORS!,
-			authCors: process.env.AUTH_CORS!,
+			storeCors: process.env.STORE_CORS,
+			adminCors: process.env.ADMIN_CORS,
+			authCors: process.env.AUTH_CORS,
 			jwtSecret: process.env.JWT_SECRET || "supersecret",
 			cookieSecret: process.env.COOKIE_SECRET || "supersecret",
 		},
