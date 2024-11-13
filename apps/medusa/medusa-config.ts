c48430fe8afdced30c7f02659d7f412a004895e7
LordPachimari <opachimari@gmail.com>
2024-11-13 11:29:41 +0300

    add empty stirngs

--- apps/medusa/medusa-config.ts
+++ apps/medusa/medusa-config.ts
@@ -29,11 +29,11 @@ export default defineConfig({
 		{
 			resolve: "./modules/sanity",
 			options: {
-				api_token: process.env.SANITY_API_TOKEN,
-				project_id: process.env.SANITY_PROJECT_ID,
+				api_token: process.env.SANITY_API_TOKEN ?? "",
+				project_id: process.env.SANITY_PROJECT_ID ?? "",
 				api_version: new Date().toISOString().split("T")[0],
 				dataset: "production",
-				studio_url: process.env.SANITY_STUDIO_URL,
+				studio_url: process.env.SANITY_STUDIO_URL ?? "",
 				type_map: {
 					collection: "collection",
 					category: "category",
