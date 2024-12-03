deb2bec67807139dde190d664982ba3a00f6e0b3
LordPachimari <opachimari@gmail.com>
2024-12-03 19:43:47 +0300

    check

--- apps/storefront/src/app/env.ts
+++ apps/storefront/src/app/env.ts
@@ -17,7 +17,8 @@ export const env = createEnv({
 		NEXT_PUBLIC_REPLICACHE_KEY: z.string(),
 		NEXT_PUBLIC_PARTYKIT_HOST: z.string(),
 	},
-	experimental__runtimeEnv: {
+	runtimeEnv: {
+		SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
 		NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
 		NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 		NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
@@ -27,6 +28,9 @@ export const env = createEnv({
 		NEXT_PUBLIC_BLAZZING_APP_WORKER_URL:
 			process.env.NEXT_PUBLIC_BLAZZING_APP_WORKER_URL,
 		NEXT_PUBLIC_PARTYKIT_HOST: process.env.NEXT_PUBLIC_PARTYKIT_HOST,
+		BLAZZING_STORE_ID: process.env.BLAZZING_STORE_ID,
+		SANITY_REVALIDATE_SECRET: process.env.SANITY_REVALIDATE_SECRET,
+		WEB_URL: process.env.WEB_URL,
 	},
 	// For Next.js >= 13.4.4, you only need to destructure client variables:
 	// experimental__runtimeEnv: {
